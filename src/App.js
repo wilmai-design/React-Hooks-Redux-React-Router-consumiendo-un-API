f885ea52451c899954af97cdff85bc9580fa8671
System Administrator <root@NTB-158.local>
2020-06-09 17:54:16 -0400

    Desarrollando el componente country y Diseñandolo con styled-Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Country from './components/country';
 
 function App() {
   return (
     <div className="App">
-      
+      <Country 
+        flag="https://cdn.countryflags.com/thumbs/spain/flag-400.png"
+        name="España"
+        population={1231231}
+        region= "Europa"
+        capital= "Madrid"
+      />
     </div>
   );
 }
